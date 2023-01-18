f4ae5cf86f0c62f0600d9d0cba30b456f8d25fdd
Caleb <calebrod1@yahoo.com>
2023-01-18 13:12:06 -0600

    variables, boolean, floats, etc.

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,20 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("\"I love pizza\\");
+        System.out.print("It's really good!\n");
+        System.out.println(":)");
+        // This is a comment
+        int x; //declare
+        x = 123; //assign
+        System.out.println("My number is: " + x);
+        int y = 321; //initialize
+        System.out.println("y = " + y);
+        float z = 3.14f;
+        double d = 3.14;
+        boolean a = true;
+        System.out.println(a);
+        char symbol = '@';
+        String name = "Bro";
+        System.out.println("Hello " + name);
     }
 }
